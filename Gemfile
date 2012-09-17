47c24b2d297809a9a54e48e635bbfb37ed8c81d6
Gene Bang <genebang@gmail.com>
2012-09-17 16:32:18 -0700

    added pagination

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jquery-rails'
 
 gem 'devise'
 
+gem 'will_paginate', '~> 3.0.0'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def index
-    @links = Link.all
+    #@links = Link.all
+    #@links = Link.paginate(:page => params[:page], :per_page => 2, :order => 'updated_at DESC')
+    # ex - Post.page(params[:page]).order('created_at DESC')
+    # ex2 - Link.order('created_at DESC').page(1).per_page(1)
+    # use params[:per_page] ||= 25 and use params[:page] ||= 1
+    #@links = Link.per_page(params[:per_page] ||= 2).page(params[:page] ||= 1).order('created_at DESC')
+    params[:per_page] ||= 2
+    params[:page] ||= 1
+    @links = Link.paginate(:page => params[:page], :per_page => params[:per_page], :order => 'created_at DESC')
   end
 end
