552783f1a89fe2dea2148bb488ac9a145c6d963e
Gene Bang <genebang@gmail.com>
2012-09-17 21:24:29 -0700

    added vote model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ActiveRecord::Base
   attr_accessible :email, :username
   has_many :links
   has_many :comments
+  has_many :votes
 end
---
+++ app/models/vote.rb
@@ -0,0 +1,6 @@
+class Vote < ActiveRecord::Base
+  # attr_accessible :title, :body
+  attr_accessible :user_id, :link_id, :up
+  belongs_to :user
+  belongs_to :link
+end
